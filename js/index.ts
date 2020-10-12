2c7a576d491c7ea1f1830d88a50c4d10d50d5f52
aguilarm <mika.aguilar@gmail.com>
2020-10-12 15:09:45 -0500

    refactor(*): Make seed for each star to determine color

--- js/index.ts
+++ js/index.ts
@@ -1,8 +1,16 @@
 import { alea as Alea } from 'seedrandom';
 
 // get random number between a and b inclusive, ie (a <= x <= b)
-function __randomIntFromInterval(min, max, rand) {
-  return Math.floor(rand() * (max - min + 1) + min);
+// function __randomIntFromInterval(min, max, rand) {
+//   return (rand() % (max - min)) + min;
+// }
+
+function resetCanvas(canvas: HTMLCanvasElement) {
+  const context2D = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  context2D.fillStyle = 'black';
+  context2D.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function drawStarField(canvas: HTMLCanvasElement, rand) {
@@ -10,9 +18,10 @@ function drawStarField(canvas: HTMLCanvasElement, rand) {
   const start = Date.now();
   for (let x = 0; x < canvas.width; x++) {
     for (let y = 0; y < canvas.height; y++) {
-      const roll = __randomIntFromInterval(0, 100, rand);
-      const isStar = roll < 5;
-      context2D.fillStyle = isStar ? 'white' : 'black';
+      const isStar = rand() < 0.005;
+      if (!isStar) continue;
+      const starGen = new Alea(x+y);
+      context2D.fillStyle = starGen() < 0.01 ? 'coral' : 'white';
       context2D.fillRect(x, y, 1, 1);
     }
   }
@@ -21,8 +30,7 @@ function drawStarField(canvas: HTMLCanvasElement, rand) {
 
 (function init() {
   const canvas = document.getElementById("game") as HTMLCanvasElement;
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  resetCanvas(canvas);
 
   const generator = new Alea('thisisrandomaf');
 
@@ -32,6 +40,7 @@ function drawStarField(canvas: HTMLCanvasElement, rand) {
     switch(event.code) {
       case 'Space':
         const newGenerator = new Alea(Date.now());
+        resetCanvas(canvas);
         drawStarField(canvas, newGenerator);
         break;
       default:
